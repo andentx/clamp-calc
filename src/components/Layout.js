895adf770ed75147decec707b26a30a4558ca3b4
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-15 16:51:48 -0500

    Add icons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,8 +14,37 @@ const Layout = ({ children }) => {
         />
         <meta name='author' content='Andrew Dent' />
 
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='shortcut icon' href='/icons/favicon.ico' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='icons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='icons/favicon-16x16.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='icons/apple-touch-icon.png'
+        />
+        <link
+          rel='mask-icon'
+          href='icons/safari-pinned-tab.svg'
+          color='hsl(210, 63%, 16%)'
+        />
+        <link rel='manifest' href='icons/site.webmanifest' />
+        <meta name='msapplication-TileColor' content='hsl(210, 63%, 16%)' />
+        <meta name='msapplication-config' content='icons/browserconfig.xml' />
+
         <meta name='theme-color' content='hsl(210, 63%, 16%)' />
       </Head>
+
       <div className={styles.pageContainer}>
         <main className={styles.mainContent}>{children}</main>
       </div>
