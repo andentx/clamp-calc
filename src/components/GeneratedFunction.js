463be91960349ed55bcc8301d027fe55d55f88d3
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-15 13:16:03 -0500

    Update generated string component

--- src/components/GeneratedFunction.js
+++ src/components/GeneratedFunction.js
@@ -1,7 +1,18 @@
-export default function GeneratedFunction() {
+import React from 'react';
+import { useGlobalContext } from '../contexts';
+
+import styles from '../styles/GeneratedFunction.module.scss';
+
+const GeneratedFunction = () => {
+  const { fullFunctionString } = useGlobalContext();
+
   return (
-    <>
-      <h1>generated function</h1>
-    </>
+    <div className={styles.SectionContainer}>
+      <div className={styles.FunctionAndButtonContainer}>
+        <p className={styles.FunctionStringContainer}>{fullFunctionString}</p>
+      </div>
+    </div>
   );
-}
+};
+
+export default GeneratedFunction;
