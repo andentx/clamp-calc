0438ce351744cae3336e22fe16e251bb4ebaff76
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-15 16:40:44 -0500

    Add meta data

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
+import Head from 'next/head';
 
 import styles from '../styles/Layout.module.scss';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Clamp Calc</title>
+        <meta
+          name='description'
+          content='Clamp Calc is a tool for generating clamp CSS functions'
+        />
+        <meta name='author' content='Andrew Dent' />
+
+        <meta name='theme-color' content='hsl(210, 63%, 16%)' />
+      </Head>
       <div className={styles.pageContainer}>
         <main className={styles.mainContent}>{children}</main>
       </div>
